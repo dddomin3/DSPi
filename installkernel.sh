3841a6124c9334d1dfb68690019025401ce1c53f
Dennis Dominguez <cheekyisgreat@gmail.com>
2017-04-02 21:25:28 -0600

    Adding installkernel.sh, which helps with kernal installation, and is referenced in README.md

---
+++ installkernel.sh
@@ -0,0 +1,11 @@
+sudo rm -r /media/cheekymusic/boot/overlays/
+sudo rm -r /media/cheekymusic/7f593562-9f68-4bb9-a7c9-2b70ad620873/lib/firmware/
+cd ~/rtkernel/boot
+sudo cp -rd * /media/cheekymusic/boot/
+cd ../lib
+sudo cp -dr * /media/cheekymusic/7f593562-9f68-4bb9-a7c9-2b70ad620873/lib/
+
+
+
+
+
