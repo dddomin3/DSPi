87693a52cf72597ba2074f68f1a688f3d074c2b7
Dennis Dominguez <cheekyisgreat@gmail.com>
2017-05-05 19:28:34 -0600

    Moving jack server start to dspi switcher, since different dspis need different jack server settings for optimization on raspi.
    Made dspiSwitcher a lot less redundant.

--- dspiSwitcher.sh
+++ dspiSwitcher.sh
@@ -1,35 +1,31 @@
 #!/bin/bash
+export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/dbus/system_bus_socket
+killall amsynth
+killall jack-rack
+killall guitarix
 [ $dspi == 'guitarix' ] && (
-  killall amsynth
-  killall jack-rack
-  killall guitarix
-  sudo ifdown wlan0 &
-  sleep 10
+  killall jackd &&
+  jackd -P80 -p16 -S -t8000 -dalsa -dhw:CODEC,0 -p64 -n6 -r48000 -s -S -Xseq -Dtrue -i >> /home/pi/DSPi/jackboot.log &
+  sudo ifdown wlan0
+  sleep 15
   guitarix --nogui >> /home/pi/DSPi/jackboot.log &
   chrt -p 75 $! &
   echo "nernerner"
   exit 0;
 )
 [ $dspi == 'amsynth' ] && (
-  killall guitarix
-  killall jack-rack
-  killall amsynth
-  sudo ifdown wlan0 &
-  sleep 10
+  killall jackd &&
+  jackd -P80 -p16 -S -t8000 -dalsa -dhw:CODEC,0 -p64 -n6 -r48000 -s -S -Xseq -i0 -Dfalse >> /home/pi/DSPi/jackboot.log &
+  sudo ifdown wlan0
+  sleep 15
   amsynth -x -malsa -ajack -c9 -p4 -r48000 >> /home/pi/DSPi/jackboot.log &
   chrt -p 75 $! &
   echo "wubwubwub"
   exit 0;
 )
-[ $dspi == 'sidechain' ] && (
-  killall guitarix
-  killall amsynth
-  killall jack-rack
-  sudo ifup wlan0 &
-  sleep 10
-  jack-rack --help >> /home/pi/DSPi/jackboot.log &
-  chrt -p 75 $! &
-  echo "unAunAunA"
+[ $dspi == 'debug' ] && (
+  sudo ifup wlan0
+  echo "Debugging" >> /home/pi/DSPi/jackboot.log
   exit 0;
 )
 exit 1;
--- jackstart.sh
+++ jackstart.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
-export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/dbus/system_bus_socket
-# sudo mount -o remount,size=128M /dev/shm #dont think i need to do this due to gpu mem being pretty low.
 sudo killall gvfsd
 sudo killall dbus-daemon
 sudo killall dbus-launch
 cd /home/pi/DSPi
-jackd -P80 -p16 -S -t8000 -dalsa -dhw:CODEC,0 -p64 -n6 -r48000 -s -S -Xseq &
-# sleep 15
-# dspi=amsynth ./dspiSwitcher.sh
-sleep 15
 aj-snapshot -d aj-snapshot.xml &
 mididings "Filter(CTRL)" &
 pd -alsamidi -midiindev 1 -nomidiout -nrt -nogui -noaudio midiDspSwitch.pd &
