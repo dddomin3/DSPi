3515e083d62eddd42bc4c64fabda23594df2d044
Raspi <cheekyisgreat@gmail.com>
2017-05-08 01:08:15 +0000

    Fixed bug where CC 127 didnt do anything.
    Made jackd server settings better
    fixed bug where chrt was giving the wlan down command rt priority
    Everything works better, yay!

--- dspiSwitcher.sh
+++ dspiSwitcher.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/dbus/system_bus_socket
-killall amsynth
-killall jack-rack
-killall guitarix
+killall -9 amsynth
+killall -9 guitarix
+killall -9 jackd
 [ $dspi == 'guitarix' ] && (
-  killall jackd &&
-  jackd -P80 -p16 -S -t8000 -dalsa -dhw:CODEC,0 -p64 -n6 -r48000 -s -S -Xseq -Dtrue -i >> /home/pi/DSPi/jackboot.log &
-  sudo ifdown wlan0
+  jackd -P80 -p16 -S -t2000 -dalsa -dhw:CODEC,0 -p64 -n6 -r48000 -s -S -Xseq -D >> /home/pi/DSPi/jackboot.log &
+  # chrt -a -r -p 80 $! &
   sleep 15
   guitarix --nogui >> /home/pi/DSPi/jackboot.log &
-  chrt -p 75 $! &
+  chrt -a -r -p 75 $! &
   echo "nernerner"
+  sudo ifdown wlan0 &
   exit 0;
 )
 [ $dspi == 'amsynth' ] && (
-  killall jackd &&
-  jackd -P80 -p16 -S -t8000 -dalsa -dhw:CODEC,0 -p64 -n6 -r48000 -s -S -Xseq -i0 -Dfalse >> /home/pi/DSPi/jackboot.log &
-  sudo ifdown wlan0
+  jackd -s -S -P80 -p16 -t2000 -dalsa -dhw:CODEC,0 -r48000 -p64 -n6 -s -S -P -Xnone >> /home/pi/DSPi/jackboot.log &
+  # chrt -a -r -p 80 $! &
   sleep 15
   amsynth -x -malsa -ajack -c9 -p4 -r48000 >> /home/pi/DSPi/jackboot.log &
-  chrt -p 75 $! &
+  chrt -a -r -p 75 $! &
   echo "wubwubwub"
+  sudo ifdown wlan0 &
   exit 0;
 )
 [ $dspi == 'debug' ] && (
-  sudo ifup wlan0
+  sudo ifup wlan0 &
   echo "Debugging" >> /home/pi/DSPi/jackboot.log
   exit 0;
 )
