5b5bcb08ecf30ae9fbd94305ea7d19eeedc82daf
Dennis Dominguez <cheekyisgreat@gmail.com>
2017-09-11 22:26:29 -0600

    Switching to using mutliple pd ports. (#27)
    This solves issues due to crosstalk due to the sheer amount of CCs/Channels used by both MFTT and Otcatrack.

--- bash/jackstart.sh
+++ bash/jackstart.sh
@@ -5,5 +5,5 @@ sudo killall dbus-launch
 cd /home/pi/DSPi/configs/
 aj-snapshot -d aj-snapshot.xml &
 cd /home/pi/DSPi/pd/
-pd -alsamidi -midiindev 1 -midioutdev 1 -nrt -nogui -noaudio midiDspSwitch.pd &
+pd -alsamidi -midiindev 0,1 -midioutdev 2,3 -nrt -nogui -noaudio midiDspSwitch.pd &
 exit 0
