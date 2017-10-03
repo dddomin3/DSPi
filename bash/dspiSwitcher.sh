a34645323a17899a41b13c89bb964a0d4c768222
Dennis Dominguez <cheekyisgreat@gmail.com>
2017-10-03 16:23:13 -0600

    Fixed a bug where a2jmidid was causing guitarix to crash if it was launched before guitarix.

--- bash/dspiSwitcher.sh
+++ bash/dspiSwitcher.sh
@@ -13,11 +13,10 @@ fi
 [ $dspi == 'guitarix' ] && (
   jackd -s -S -P80 -p16 -t2000 -dalsa -dhw:CODEC,0 -r48000 -p64 -n6 -s -S -D -Xnone >> /home/pi/DSPi/jackboot.log &
   # chrt -a -r -p 80 $! &
-  sleep 10
-  a2jmidid &
-  sleep 10
+  sleep 15
   guitarix --nogui -t >> /home/pi/DSPi/jackboot.log &
-  chrt -a -r -p 75 $! &
+  sleep 25
+  a2jmidid &
   echo "nernerner"
   sudo ifdown wlan0 &
   exit 0;
