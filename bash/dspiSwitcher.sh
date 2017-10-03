4f75ca2842b51f0fd209a51306a0e5c00f93ee85
Dennis Dominguez <cheekyisgreat@gmail.com>
2017-10-03 16:03:51 -0600

    Adding a sleep between a2jmidid and guitarix

--- bash/dspiSwitcher.sh
+++ bash/dspiSwitcher.sh
@@ -2,8 +2,8 @@
 export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/dbus/system_bus_socket
 killall -9 amsynth
 killall -9 guitarix
-killall -9 jackd
 killall -9 a2jmidid
+killall -9 jackd
 
 # This checks to see if an arguement was passed in. This overwrites the dspi environment variable
 if [ "${#1}" -gt "0" ]; then
@@ -13,8 +13,9 @@ fi
 [ $dspi == 'guitarix' ] && (
   jackd -s -S -P80 -p16 -t2000 -dalsa -dhw:CODEC,0 -r48000 -p64 -n6 -s -S -D -Xnone >> /home/pi/DSPi/jackboot.log &
   # chrt -a -r -p 80 $! &
-  sleep 15
+  sleep 10
   a2jmidid &
+  sleep 10
   guitarix --nogui -t >> /home/pi/DSPi/jackboot.log &
   chrt -a -r -p 75 $! &
   echo "nernerner"
